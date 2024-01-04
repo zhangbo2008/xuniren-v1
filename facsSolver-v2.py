028a097612b3bf52c9ddf5da91dec910b3266aa4
陆延杰 <1290851757@qq.com>
2024-01-05 00:19:30 +0800

    fix v2 audio2face issue

--- facsSolver-v2.py
+++ facsSolver-v2.py
@@ -204,17 +204,20 @@ class FacsSolver:
 
         outWeight = outWeight * (outWeight > 1.0e-9)
         # print (outWeight)
-        outWeight = outWeight * (outWeight > 1.0e-9)
-        # print (outWeight)
- 
         blend=["eyeBlinkLeft", "eyeLookDownLeft", "eyeLookInLeft", "eyeLookOutLeft", "eyeLookUpLeft", "eyeSquintLeft", "eyeWideLeft", "eyeBlinkRight", "eyeLookDownRight", "eyeLookInRight", "eyeLookOutRight", "eyeLookUpRight", "eyeSquintRight", "eyeWideRight", "jawForward", "jawLeft", "jawRight", "jawOpen", "mouthClose", "mouthFunnel", "mouthPucker", "mouthLeft", "mouthRight", "mouthSmileLeft", "mouthSmileRight", "mouthFrownLeft", "mouthFrownRight", "mouthDimpleLeft", "mouthDimpleRight", "mouthStretchLeft", "mouthStretchRight", "mouthRollLower", "mouthRollUpper", "mouthShrugLower", "mouthShrugUpper", "mouthPressLeft", "mouthPressRight", "mouthLowerDownLeft", "mouthLowerDownRight", "mouthUpperUpLeft", "mouthUpperUpRight", "browDownLeft", "browDownRight", "browInnerUp", "browOuterUpLeft", "browOuterUpRight", "cheekPuff", "cheekSquintLeft", "cheekSquintRight", "noseSneerLeft", "noseSneerRight", "tongueOut"]
-        client=udp_client.SimpleUDPClient('127.0.0.1',5009)
+        client=udp_client.SimpleUDPClient('127.0.0.1',5008)
         osc_array=outWeight.tolist()
-        count=0
-        for i in osc_array:
-            client.send_message('/'+str(blend[count]),i)
-            count+=1
+        
+        messagex=str(osc_array).replace("[","").replace("]","")
+        print(str(messagex))
+        client.send_message(str(messagex),0)
 
+        #count=0
+        #for i in osc_array:
+         #   client.send_message('/'+str(blend[count]),i)
+          #  count+=1
+         #   print(str(blend[count]))
+         #   print(i)
 
         return outWeight
 
